9d45aec3e4dc3f81c1151e3e60c250153e2fd72e
zhangchaoqin <chaoqin.zhang@novauto.com.cn>
2023-11-02 23:06:56 +0800

    garb

---
+++ garb.v
@@ -0,0 +1,33 @@
+module garb (  /*AUTOARG*/ );
+
+  //=============================================================================
+  //  parameter declare
+  //=============================================================================
+
+
+  //=============================================================================
+  //  interface declare
+  //=============================================================================
+  /*AUTOTIEOFF*/
+  input clk;    // Clock
+  input rst_n;  // Asynchronous reset active low
+
+  output [5:0] unused;
+  
+
+  //=============================================================================
+  //  wire/reg declaration
+  //=============================================================================
+  /*AUTOINPUT*/
+  /*AUTOWIRE*/
+  /*AUTOREG*/
+
+
+endmodule
+// Local Variables:
+// verilog-auto-inst-param-value:t
+// indent-tabs-mode:nil
+// tab-width:2
+// verilog-library-directories:(".")
+// verilog-library-extensions:(".v")
+// End:
